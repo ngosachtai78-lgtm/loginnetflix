2aeb0abd061f920923c3d1edfa47d74a4a849c83
Macpro m1 <macprom1@Sach-Tai-ayy.local>
2025-09-20 09:36:20 +0700

    bypass SSL check for Render

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+// Bỏ qua kiểm tra SSL để fetch tới HTTPS không hợp lệ
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
 
 const express = require('express');
 const session = require('express-session');
